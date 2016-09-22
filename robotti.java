c98c8e9e4807eb9acb23b3a37f855f6b2a84fad3
pekustyle <pekustyle@gmail.com>
2016-09-22 12:11:00 +0300

    Robotti.java
    lol

---
+++ robotti.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import robotti.Ohjain;
+ 
+public class Paaohjelma {
+    public static void main(String[] args) {
+ 
+        // käynnistetään robotti
+        Ohjain.kaynnista();
+ 
+        Ohjain.ylos();
+        Ohjain.ylos();
+        Ohjain.ylos();
+        Ohjain.ylos();
+        Ohjain.ylos();
+        Ohjain.oikealle();
+        Ohjain.oikealle();
+        Ohjain.oikealle();
+        Ohjain.oikealle();
+        Ohjain.oikealle();
+        Ohjain.oikealle();
+        Ohjain.oikealle();
+        Ohjain.alas();
+        Ohjain.alas();
+        Ohjain.alas();
+        Ohjain.alas();
+        Ohjain.alas();
+        Ohjain.vasemmalle();
+        Ohjain.vasemmalle();
+        Ohjain.vasemmalle();
+        Ohjain.vasemmalle();
+        Ohjain.vasemmalle();
+        Ohjain.vasemmalle();
+        Ohjain.vasemmalle();
+        Ohjain.ylos();
+        Ohjain.ylos();
+        Ohjain.ylos();
+        Ohjain.oikealle();
+        Ohjain.oikealle();
+        Ohjain.oikealle();
+        Ohjain.ylos();
+        Ohjain.ylos();
+        Ohjain.vasemmalle();
+        Ohjain.vasemmalle();
+        Ohjain.vasemmalle();
+         
+         
+         
+         
+         
+       
+ 
+        // sammutetaan robotti
+        Ohjain.sammuta();
+    }
+}
